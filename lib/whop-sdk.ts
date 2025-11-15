53b4833b0cbc20eb5883e3a4c96e25208613562e
hussain poonawala <hussain.poonawala@gdn-commerce.com>
2025-11-15 11:20:42 +0530

    Added new app webhook

--- lib/whop-sdk.ts
+++ lib/whop-sdk.ts
@@ -7,7 +7,7 @@ export const whopsdk = new Whop({
 });
 
 export const whopsdkApp = new Whop({
-	appID: process.env.NEXT_PUBLIC_WHOP_APP_ID_APP,
-	apiKey: process.env.WHOP_API_KEY_APP,
+	appID: process.env.NEXT_PUBLIC_WHOP_APP_ID,
+	apiKey: process.env.WHOP_API_KEY,
 	webhookKey: btoa(process.env.WHOP_WEBHOOK_SECRET_APP || ""),
 });
